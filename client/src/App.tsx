e5647e8068ab1b05398e657d98163f464fc4a466
Tejas Mahajan <trm2468@gmail.com>
2023-10-17 19:53:39 +0530

    Websocket update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,16 +2,30 @@ import { useState } from 'react'
 
 function App() {
   const [logs, setLogs] = useState([])
+  const socket = new WebSocket('ws://localhost:3000');
 
-  const handleFetch = async(e) =>{
-    e.preventDefault();
-    const response = await fetch("http://localhost:3000/logs");
-    const newlogs = await response.json();
-    console.log(logs)
-    setLogs((prevlogs)=>
-      [...prevlogs, ...newlogs]
-    )
-  }
+  // const handleFetch = async(e) =>{
+  //   e.preventDefault();
+  //   const response = await fetch("http://localhost:3000/logs");
+  //   const newlogs = await response.json();
+  //   console.log(logs)
+  //   setLogs((prevlogs)=>
+  //     [...prevlogs, ...newlogs]
+  //   )
+  // }
+
+  useEffect(() => {
+    socket.onmessage = (event) => {
+      const receivedLogs = JSON.parse(event.data);
+      setLogs((prevlogs) => [...prevLogs, ...receivedLogs]);
+    };
+
+    return () => {
+      socket.close();
+    };
+
+  }, []);
+
   return (
     <>
     <div class="container-sm">
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const cors = require('cors')
+const WebSocket = require('ws');
 const fs = require('fs')
 
 app.use(cors())
@@ -13,16 +14,33 @@ app.use(express.json());
 // }
   
 // app.use(myLogger)
+const wss = new WebSocket.Server({noServer: true});
+const clients = new Set();
+
+app.server.on('upgrade', (request,socket,head) => {
+    wss.handleUpgrade(request, socket, head, (ws) => {
+        wss.emit('connection', ws, request);
+    });
+});
+
+wss.on('connection', (ws) => {
+    clients.add(ws);
+
+    ws.on('close', () => {
+        clients.delete(ws);
+    });
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/logs', (req,res) => {
-    fs.readFile('./logs.json', (err, json) => {
-        let obj = JSON.parse(json);
-        res.json(obj);
-    });
-})
+// app.get('/logs', (req,res) => {
+//     fs.readFile('./logs.json', (err, json) => {
+//         let obj = JSON.parse(json);
+//         res.json(obj);
+//     });
+// })
 
 app.post('/ingest', (req,res) => {
     let newlogs = req.body;
@@ -35,8 +53,18 @@ app.post('/ingest', (req,res) => {
     }
     logsjson = JSON.stringify(logs);
     fs.writeFileSync("logs.json",logsjson,"utf-8");
+    broadcastLogs(logsjson);
     res.send("Logs written");
 })
+
+function broadcastLogs(logs) {
+    const logData = JSON.stringify(logs);
+    clients.forEach((client) =>{
+        if(client.readyState === WebSocket.OPEN) {
+            client.send(logData);
+        }
+    })
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
